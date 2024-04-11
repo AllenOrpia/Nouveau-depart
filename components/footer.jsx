af10795b36f12771bf1b4916f7d48dc2b7fa6186
allenOrpia <118657615+allenOrpia@users.noreply.github.com>
2024-04-11 00:26:05 -0700

    first commit

---
+++ components/footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='container mx-auto bg-blue-500'>Footer</footer>
+  )
+}
+
+export default Footer
---
+++ components/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='container mx-auto bg-blue-500'>
+        Header
+        <div id="socials"></div>
+        <div id="logo"></div>
+        <div id="links"></div>
+    </header>
+  )
+}
+
+export default Header
